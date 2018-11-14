6fe91f20e047d1927e86f92de7d568b597d67884
Brandon Horst <brandonhorst@gmail.com>
2018-11-13 23:27:48 -0500

    Use @_cdecl in Swift, so that mangled names do not need to be regenerated when the signature changes

--- NativeExtensionInSwift/Sources/NativeExtensionInSwift/NativeExtensionInSwift.swift
+++ NativeExtensionInSwift/Sources/NativeExtensionInSwift/NativeExtensionInSwift.swift
@@ -1,15 +1,14 @@
-public struct NativeExtensionInSwift {
-    
-    public func fibonacci(_ n: CInt) -> CInt {
-        if n == 0 {
-            return 0
-        } else if n == 1{
-            return 1
-        }
-        return fibonacci(n - 1) + fibonacci(n - 2)
+@_cdecl("swift_fibonacci")
+public func fibonacci(_ n: CInt) -> CInt {
+    if n == 0 {
+        return 0
+    } else if n == 1{
+        return 1
     }
-    
-    public func printHello() {
-        print("hello")
-    }
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+@_cdecl("swift_print_hello")
+public func printHello() {
+    print("hello")
 }
